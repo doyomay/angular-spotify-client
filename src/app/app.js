08f12f0ffc8e4e23afe2fa2d2952ff0fd5d03641
Carlos Gerardo May Esquivel <carlos.may@globant.com>
2017-08-08 15:41:52 -0500

    primer commit

---
+++ src/app/app.js
@@ -0,0 +1,29 @@
+import angular from 'angular';
+
+import '../style/app.css';
+
+let app = () => {
+    return {
+        template: require('./app.html'),
+        controller: 'AppCtrl',
+        controllerAs: 'app'
+    }
+};
+
+class AppCtrl {
+    constructor($scope) {
+        this.url = 'https://github.com/preboot/angular-webpack';
+        $scope.nombre = "";
+        $scope.hey = () => {
+            alert($scope.nombre);
+        }
+    }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+    .directive('app', app)
+    .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
